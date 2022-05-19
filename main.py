ea3d7c1caca3b6df793371be6b42f1cbf05b8fc2
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-05-19 15:59:20 +0200

    Added Users

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
 
-from model import User
+from model import Gender, Role, User
 
 app = FastAPI()
 
 db: List[User] = [
-
+    User(
+        id=uuid4(),
+        first_name="Selena",
+        last_name="Gomez",
+        gender=Gender.female,
+        roles=[Role.admin]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Vishnu",
+        last_name="Viswambharan",
+        gender=Gender.male,
+        roles=[Role.user]
+    )
 ]
 
 
